2499827dab7ce76221c9ee936f751271ce229e93
Moyanogdev <gmoyanodvlpr@gmail.com>
2023-01-26 18:06:35 -0300

    switch&Fonts

---
+++ src/screens/GameScreen.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import Card from '../components/Card'
+import colors from '../constants/colors'
+
+const GameScreen = () => {
+    const [currentGuess, setCurrentGuess] = useState()
+
+    useEffect(() => {
+        setCurrentGuess(Math.floor(Math.random() * (99 - 1) + 1))
+    }, [])
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textColor}>Guess the number</Text>
+      <Text style={styles.textColor}>{currentGuess}</Text>
+      <Card newStyles={styles.buttonContainer}>
+        <Button title='Minor' onPress={() => console.log('Minor')}/>
+        <Button title='Mayor' onPress={() => console.log('Mayor')}/>
+      </Card>
+    </View>
+  )
+}
+
+export default GameScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+        backgroundColor: colors.primary,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 20,
+        width: '80%',
+    },
+    textColor: {
+        color: 'white',
+    }
+})
